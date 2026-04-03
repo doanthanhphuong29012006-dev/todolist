1a76e6df81e4596e9fd75940c5c374a41a7ab3a5
doanthanhphuong29012006-dev <doanthanhphuong29012006@gmail.com>
2026-04-03 20:22:36 +0700

    remove env

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import pg from 'pg';
 const { Pool } = pg;
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
