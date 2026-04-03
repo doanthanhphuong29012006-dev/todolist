17e1900f7d4a3286b135a3ad1a5b891a7818f9f3
doanthanhphuong29012006-dev <doanthanhphuong29012006@gmail.com>
2026-04-03 20:02:55 +0700

    deploy = vercel

--- db.js
+++ db.js
@@ -1,24 +1,13 @@
-import 'dotenv/config'
-import pg from 'pg'
-
-const { Pool } = pg
+import pg from 'pg';
+const { Pool } = pg;
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-})
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-pool.connect((err, client, release) => {
-    if (err) {
-        console.err('Lỗi kết nối db: ', err.stack)
-    } else  {
-        console.log('DB ok rồi hehe')
-    }
-
-    if (client) release()
-})
-
-export default pool
+export default pool;
