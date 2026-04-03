38eefe50b86cf2b7b614320d03d0e30e588b1bd4
doanthanhphuong29012006-dev <doanthanhphuong29012006@gmail.com>
2026-04-03 20:26:06 +0700

    fix path

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import pool from './db.js'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import path from 'path'
-import { fileURLToPath } from 'url'
-console.log("Đường dẫn hiện tại nè:", __dirname)
 
 const app = express()
 const Port = 3000
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 import authRoutes from './routes/auth.js'
 import todoRoutes from './routes/todos.js'
